a00034beb1f1376dd750a7f06363154d7f102ef1
Matt Duffield <duffieldml@gmail.com>
2021-01-13 23:23:29 -0500

    Worked out uv index api call

--- Assets/script.js
+++ Assets/script.js
@@ -18,21 +18,38 @@ function createButtons() {
 
 // function getWeather() {
 //     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKEY + "&units=imperial";
+
+//     // Perfoming an AJAX GET request to our queryURL
 //     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//       })
-
+//       url: queryURL,
+//       method: "GET"
+//     })
 //         .then(function (response) {
-//             //$("#list-group").html("")
-//             var weatherDiv = $("<div class='currentWeather'>");
-//             weatherDiv.html("<h2>Current Weather for: " + city);
-
 //             console.log(response);
-//         })
-        
-// }
+//             if (searchedCities.includes(response.name) === false ) {
+//                 searchedCities.push(response.name);
+//             }
+//             $("#searched-city").empty();
+//             $("#current-weather-container").empty();
+//             var cityDiv = $("<div>");
+//             cityDiv.html("Current forecast for " + response.name);
+//             //$("#searched-city").prepend(cityDiv);
+//             var p = $("<p>").html("<i>" + todayDate + "</i>");
+//             var p1 = $("<p>").html("<i> Temperature: " + response.main.temp.toFixed(0) + "&deg;F" + "</i>");
+//             var p2 = $("<p>").html("<i> Humidity: " + response.main.humidity.toFixed(0) + "%" + "</i>");
+//             var p3 = $("<p>").html("<i> Wind Speed: " + response.wind.speed.toFixed(0) + " MPH" + "</i>");
+//             //var p4 = $("<p>").html("<i> UV Index: " + response.wind.speed.toFixed(0) + " MPH" + "</i>");      This still needs to be done
+//             var iconImage = $("<img id = 'icon'>");
+//             $("#searched-city").append(iconImage);
+//             var iconEl = response.weather[0].icon;
+//             var iconURL = "http://openweathermap.org/img/w/" + iconEl + ".png";
+//             $('#icon').attr('src', iconURL);
+//             $("#searched-city").prepend(cityDiv);
+//             $("#current-weather-container").append(p, p1, p2, p3);
 
+//             createButtons();
+//         });
+//     }
 
 
 
@@ -54,23 +71,46 @@ $("#run-search").on("click", function (event) {
             }
             $("#searched-city").empty();
             $("#current-weather-container").empty();
+            
             var cityDiv = $("<div>");
             cityDiv.html("Current forecast for " + response.name);
-            $("#searched-city").prepend(cityDiv);
+            //$("#searched-city").prepend(cityDiv);
+            
             var p = $("<p>").html("<i>" + todayDate + "</i>");
             var p1 = $("<p>").html("<i> Temperature: " + response.main.temp.toFixed(0) + "&deg;F" + "</i>");
             var p2 = $("<p>").html("<i> Humidity: " + response.main.humidity.toFixed(0) + "%" + "</i>");
             var p3 = $("<p>").html("<i> Wind Speed: " + response.wind.speed.toFixed(0) + " MPH" + "</i>");
-            var p4 = $("<p>").html("<i> UV Index: " + response.wind.speed.toFixed(0) + " MPH" + "</i>");
+            
+            var iconImage = $("<img id = 'icon'>");
+            $("#searched-city").append(iconImage);
+            var iconEl = response.weather[0].icon;
+            var iconURL = "http://openweathermap.org/img/w/" + iconEl + ".png";
+
+            $('#icon').attr('src', iconURL);
+            $("#searched-city").prepend(cityDiv);
             $("#current-weather-container").append(p, p1, p2, p3);
 
-            console.log(response.main.temp);
-            console.log(response.main.humidity);
-            console.log(response.wind.speed);
+            var lon = response.coord.lon;
+            var lat = response.coord.lat;
+
+
+            var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKEY;
+
+            // Perfoming an AJAX GET request to our queryURL
+            $.ajax({
+              url: uvURL,
+              method: "GET"
+            })
+                .then(function (response) {
+                    var uvIndex = response.value;
+                    var p4 = $("<p>").html("<i> UV Index: " + uvIndex + "</i>");
+                    $("#current-weather-container").append(p4);
+                    console.log(response.value);
+                });
+
 
             createButtons();
         });
 
 })
-
 
