b2ddfe4ab5a587cb260b03ef7b2e22c9bc258be6
Matt Duffield <duffieldml@gmail.com>
2021-01-13 19:31:51 -0500

    Working on API for forecast

--- Assets/script.js
+++ Assets/script.js
@@ -3,8 +3,8 @@ let searchCity = $("#current-forecast")
 let searchedCities = [];
 let city
 let APIKEY = "7ca96097be63f6f51d3f8ccd3a2f5564";
-let presentTime = moment();
-let todayDate = moment().format("dddd, MMMM Do, YYYY");
+//let presentTime = moment();
+let todayDate = moment().format("dddd, MMMM Do, h:mm a");
 
 function createButtons() {
     $("#past-search-buttons").empty();
@@ -17,22 +17,22 @@ function createButtons() {
     }
 }
 
-function getWeather() {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKEY + "&units=imperial";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
+// function getWeather() {
+//     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKEY + "&units=imperial";
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//       })
 
-        .then(function (response) {
-            //$("#list-group").html("")
-            var newDiv = $("<div class='currentWeather'>");
-            newDiv.html("<h2>Current Weather for: " + city);
+//         .then(function (response) {
+//             //$("#list-group").html("")
+//             var weatherDiv = $("<div class='currentWeather'>");
+//             weatherDiv.html("<h2>Current Weather for: " + city);
 
-            console.log(response);
-        })
+//             console.log(response);
+//         })
         
-}
+// }
 
 
 
@@ -53,10 +53,21 @@ $("#run-search").on("click", function (event) {
             if (searchedCities.includes(response.name) === false ) {
                 searchedCities.push(response.name);
             }
-            //$("#list-group").html("")
-            var newDiv = $("<div class='currentWeather'>");
-            newDiv.html("<h2>Current Weather for: " + city);
-            console.log(todayDate)
+            $("#searched-city").html("")
+            var cityDiv = $("<div>");
+            cityDiv.html("Current forecast for " + response.name);
+            $("#searched-city").prepend(cityDiv);
+            var p = $("<p>").html("<i>" + todayDate + "</i>");
+            var p1 = $("<p>").html("<i> Temperature: " + response.main.temp.toFixed(0) + "&deg;F" + "</i>");
+            var p2 = $("<p>").html("<i> Humidity: " + response.main.humidity.toFixed(0) + "%" + "</i>");
+            var p3 = $("<p>").html("<i> Wind Speed: " + response.wind.speed.toFixed(0) + " MPH" + "</i>");
+            var p4 = $("<p>").html("<i> UV Index: " + response.wind.speed.toFixed(0) + " MPH" + "</i>");
+            $("#current-weather-container").append(p, p1, p2, p3);
+
+            console.log(response.main.temp);
+            console.log(response.main.humidity);
+            console.log(response.wind.speed);
+
             createButtons();
         });
 
