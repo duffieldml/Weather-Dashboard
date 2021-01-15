1b7587b7ef5837bc0f33a8ccd10c005cd714ddb5
Matt Duffield <duffieldml@gmail.com>
2021-01-14 23:08:00 -0500

    Completed the 5 day forecast

--- Assets/script.js
+++ Assets/script.js
@@ -114,38 +114,58 @@ function fiveDay() {
     })
         .then(function (response) {
             
-            $("#fiveday-container").empty();
-            
-
             var date1 = new Date (response.list[2].dt_txt);
             var date2 = new Date (response.list[10].dt_txt);
+            var date3 = new Date (response.list[18].dt_txt);
+            var date4 = new Date (response.list[26].dt_txt);
+            var date5 = new Date (response.list[34].dt_txt);
+
+            var img1URL = "http://openweathermap.org/img/w/" + response.list[2].weather[0].icon + ".png";
+            var img2URL = "http://openweathermap.org/img/w/" + response.list[10].weather[0].icon + ".png";
+            var img3URL = "http://openweathermap.org/img/w/" + response.list[18].weather[0].icon + ".png";
+            var img4URL = "http://openweathermap.org/img/w/" + response.list[26].weather[0].icon + ".png";
+            var img5URL = "http://openweathermap.org/img/w/" + response.list[34].weather[0].icon + ".png";
 
             var temp1 = response.list[2].main.temp.toFixed(0);
             var temp2 = response.list[10].main.temp.toFixed(0);
+            var temp3 = response.list[18].main.temp.toFixed(0);
+            var temp4 = response.list[26].main.temp.toFixed(0);
+            var temp5 = response.list[34].main.temp.toFixed(0);
 
             var hum1 = response.list[2].main.humidity.toFixed(0);
             var hum2 = response.list[10].main.humidity.toFixed(0);
+            var hum3 = response.list[18].main.humidity.toFixed(0);
+            var hum4 = response.list[26].main.humidity.toFixed(0);
+            var hum5 = response.list[34].main.humidity.toFixed(0);
 
-            var forecastDiv1 = $("<div class= card sm-primary text-light m-2>").attr("id", "day1");
-            var forecastDiv2 = $("<div class= card sm-primary text-light m-2>").attr("id", "day2");
+            $(".date1").html(moment(date1).format("dddd, hA"));
+            $(".date2").html(moment(date2).format("dddd, hA"));
+            $(".date3").html(moment(date3).format("dddd, hA"));
+            $(".date4").html(moment(date4).format("dddd, hA"));
+            $(".date5").html(moment(date5).format("dddd, hA"));
 
-            forecastDiv1.html(moment(date1).format("dddd, hA"));
-            forecastDiv2.html(moment(date2).format("dddd, hA"));
+            $(".img1").attr("src", img1URL);
+            $(".img2").attr("src", img2URL);
+            $(".img3").attr("src", img3URL);
+            $(".img4").attr("src", img4URL);
+            $(".img5").attr("src", img5URL);
 
-            $("#fiveday-container").prepend(forecastDiv1, forecastDiv2);
+            $(".temp1").html("Temperature: " + temp1 + "&deg;F");
+            $(".temp2").html("Temperature: " + temp2 + "&deg;F");
+            $(".temp3").html("Temperature: " + temp3 + "&deg;F");
+            $(".temp4").html("Temperature: " + temp4 + "&deg;F");
+            $(".temp5").html("Temperature: " + temp5 + "&deg;F");
 
-            var day1 = $("<p>").html("</b>" + "</br>" + "Temp: " + temp1 + " F </br>" + "Humidity: " + hum1 + "%");
-            var day2 = $("<p>").html("</b>" + "</br>" + "Temp: " + temp2 + " F </br>" + "Humidity: " + hum2 + "%");
+            $(".hum1").html("Humidity: " + hum1 + "%");
+            $(".hum2").html("Humidity: " + hum2 + "%");
+            $(".hum3").html("Humidity: " + hum3 + "%");
+            $(".hum4").html("Humidity: " + hum4 + "%");
+            $(".hum5").html("Humidity: " + hum5 + "%");
 
-            $("#day1").append(day1);
-            $("#day2").append(day2);
-
-            // var img1 = "http://openweathermap.org/img/w/" + response.list[2].weather[0].icon + ".png";
-
-            console.log(response);
-            console.log(date1);
-            console.log(temp1);
-            console.log(hum1);
+            // console.log(response);
+            // console.log(date1);
+            // console.log(temp1);
+            // console.log(hum1);
             // console.log(img1);
         })
 }
